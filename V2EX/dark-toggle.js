5bd61f0f3f3270f3065f2c3a77608a63854520d8
gaoyang <xz@lgysh.cn>
2023-07-14 15:56:06 +0800

    publish v2ex script.

--- V2EX/dark-toggle.js
+++ V2EX/dark-toggle.js
@@ -1,23 +1,25 @@
 // ==UserScript==
-// @name         v2ex 夜间模式自动切换
+// @name         v2ex 夜间模式跟随系统自动切换
 // @namespace    https://github.com/gaoyang/tampermonkey-scripts
-// @version      0.1
-// @description  v2ex 夜间模式自动切换
+// @version      1.0
+// @description  v2ex 夜间模式跟随系统自动切换，需要先登录才能拥有夜间模式!
 // @author       gaoyang
 // @match        https://www.v2ex.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=v2ex.com
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 ;(function () {
   'use strict'
+  
+  const toggleBtn = document.getElementsByClassName('light-toggle')[0]
+  if(!toggleBtn) return
+  const isLight = toggleBtn.children[0].src.includes('light')
 
-  let toggleBtn = document.getElementsByClassName('light-toggle')[0]
-  let isLight = toggleBtn.children[0].src.includes('light')
-
-  let media = window.matchMedia('(prefers-color-scheme: dark)')
-  let callback = e => {
-    let prefersDarkMode = e.matches
+  const media = window.matchMedia('(prefers-color-scheme: dark)')
+  const callback = e => {
+    const prefersDarkMode = e.matches
     if ((prefersDarkMode && isLight) || (!prefersDarkMode && !isLight)) {
       toggleBtn.click()
     }
